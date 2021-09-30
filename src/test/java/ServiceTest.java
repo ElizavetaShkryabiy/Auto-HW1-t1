aa1eaa3355b6f45f4ad67e1c3c50d96f6e4de66d
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-09-30 10:38:00 +0400

    junit4 added

--- src/test/java/ServiceTest.java
+++ src/test/java/ServiceTest.java
@@ -1,33 +1,35 @@
-import org.testng.annotations.Test;
+import org.junit.Test;
+
 import ru.netology.CashbackHackerService;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.*;
+
 
 public class ServiceTest {
     CashbackHackerService service = new CashbackHackerService();
 
     @Test
-    public void shouldFindRestSumToBuyWhenSumIsOK() {
+    public void shouldFindRestSumToBuyWhenSumIs1000() {
         int actual = service.remain(1000);
-        assertEquals(actual, 1000);
+        assertEquals(0, actual);
     }
 
     @Test
     public void shouldFindRestSumToBuyWhenSumBigger() {
         int actual = service.remain(1001);
-        assertEquals(actual, 999);
+        assertEquals(999, actual);
     }
 
     @Test
     public void shouldFindRestSumToBuyWhenSumTwiceBigger() {
         int actual = service.remain(2000);
-        assertEquals(actual, 1000);
+        assertEquals(0, actual);
     }
 
     @Test
     public void shouldFindRestSumToBuyWhenSumSmaller() {
         int actual = service.remain(999);
-        assertEquals(actual, 1);
+        assertEquals(1, actual);
     }
 
 }
